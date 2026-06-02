a1a2146a1dc8e13404661dfbf19ef67ea63b51c3
tomek-uvicuo <tomek@uvicuo.com>
2026-06-03 00:39:33 +0200

    fix: package setup

--- src/test/java/com/tgod/training_analytics/adapters/main/TrainingAnalyticsApplicationTests.java
+++ src/test/java/com/tgod/training_analytics/adapters/main/TrainingAnalyticsApplicationTests.java
@@ -1,6 +1,5 @@
-package com.tgod.training_analytics;
+package com.tgod.training_analytics.adapters.main;
 
-import com.tgod.training_analytics.adapters.main.TrainingAnalyticsApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
