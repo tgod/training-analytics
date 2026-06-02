e38e0b5ed1cebeb6362991936802e5001508fee1
tomek-uvicuo <tomek@uvicuo.com>
2026-06-03 00:40:53 +0200

    insights: api first

--- src/main/java/com/tgod/training_analytics/adapters/trainings/controller/AnalysisController.java
+++ src/main/java/com/tgod/training_analytics/adapters/trainings/controller/AnalysisController.java
@@ -1,9 +1,16 @@
 package com.tgod.training_analytics.adapters.trainings.controller;
 
+import com.example.api.InsightsApi;
+import com.example.model.InsightDto;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AnalysisController {
+public class AnalysisController implements InsightsApi {
 
 
+    @Override
+    public ResponseEntity<InsightDto> getInsight(Long id) {
+        return null;
+    }
 }
