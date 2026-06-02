b74c1b11864303937b37a03dfd77764bd9c12dca
tomek-uvicuo <tomek@uvicuo.com>
2026-06-03 00:37:43 +0200

    feat: trainings insights endpoint

---
+++ src/main/java/com/tgod/training_analytics/adapters/trainings/controller/AnalysisController.java
@@ -0,0 +1,9 @@
+package com.tgod.training_analytics.adapters.trainings.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AnalysisController {
+
+
+}
